682c9a3900dae7308e4b30bda4a49b7196f1d935
Intelli Pride <intellipride@gmail.com>
2024-06-24 19:10:48 +0530

    first commit

---
+++ Team28NewProject/src/com/team28/aboutus/AboutUsClass.java
@@ -0,0 +1,15 @@
+package com.team28.aboutus;
+
+public class AboutUsClass {
+	String myHouseNo;
+	double housePrice;
+	int age;
+	
+	public void firstMethod(String str) {
+		System.out.println("This is from firstMethod ");
+	}
+	
+	public void secondMethod(String str) {
+		System.out.println("This is from secondMethod ");
+	}
+}
